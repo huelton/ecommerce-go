f1a0eb487f161ba1f4005783318842dfbc708e4c
hueltonsantos <huelton.santos.ext@vr.com.br>
2025-05-04 16:38:19 -0300

    create middleware autentication

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"net/http"
+	"strings"
+)
+
+var jwtkey = []byte("minha_chave_secreta")
+
+func Autenticado() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		auth := c.GetHeader("Authorization")
+		if auth == "" || !strings.HasPrefix(auth, "Bearer ") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Token is informed"})
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(auth, "Bearer ")
+		token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+			return jwtkey, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Token is invalid"})
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		c.Set("user_id", int(claims["user_id"].(float64)))
+		c.Set("is_admin", claims["is_admin"].(bool))
+
+		c.Next()
+	}
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"ecommerce-go/controllers"
+	"ecommerce-go/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,4 +10,10 @@ func RegisterRoutes(r *gin.Engine) {
 	api := r.Group("/api")
 	api.POST("/register", controllers.Register)
 	api.POST("/login", controllers.Login)
+
+	api.GET("/me", middleware.Autenticado(), func(c *gin.Context) {
+		userID := c.GetInt("user_id")
+		isAdmin := c.GetBool("is_admin")
+		c.JSON(200, gin.H{"user_id": userID, "is_admin": isAdmin})
+	})
 }
