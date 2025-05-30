abaad0221bed4ba8efbb829719a9367886fc05bc
hueltonsantos <huelton.santos.ext@vr.com.br>
2025-05-30 20:58:35 -0300

    Integration new Relic

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
-//@SecurityDefinitions.apikey BearerAuth
-//@in header
-//@name Authorization
-//@description Informe o token JWT no formato: Bearer {seu_token}
 import (
 	"ecommerce/config"
 	"ecommerce/docs"
 	"ecommerce/routes"
 	"github.com/gin-gonic/gin"
-	"github.com/swaggo/files"
-	"github.com/swaggo/gin-swagger"
+	"github.com/newrelic/go-agent/v3/integrations/nrgin"
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"log"
 )
 
 func main() {
-	// programmatically set swagger info
+	// Inicializa o New Relic
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("Ecommerce-Golang"),
+		newrelic.ConfigLicense("804eea2d000fca630fc87e9fbccf62b7FFFFNRAL"), // use variável de ambiente
+		newrelic.ConfigDistributedTracerEnabled(true),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Swagger
 	docs.SwaggerInfo.Title = "Sistema Ecommerce GOLANG"
 	docs.SwaggerInfo.Description = "This is a sample server Ecommerce server."
 	docs.SwaggerInfo.Version = "1.0"
@@ -22,26 +32,39 @@ func main() {
 	docs.SwaggerInfo.BasePath = "/api"
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
+	// Inicializa banco de dados
 	config.ConectDataBase()
+
+	// Inicializa servidor
 	r := gin.Default()
 	r.Use(CORSMiddleware())
+
+	// Adiciona middleware do New Relic
+	r.Use(nrgin.Middleware(app))
+
+	// Rotas
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
+	// Rotas da aplicação
 	routes.RegisterRoutes(r)
 
-	r.Run(":8080")
+	// Inicia servidor
+	log.Println("Servidor iniciado em http://localhost:8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Erro ao iniciar servidor: %v", err)
+	}
 }
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // Permite qualquer origem (use com cautela)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, x-api-key")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
-
 		c.Next()
 	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -4,10 +4,24 @@ import (
 	"ecommerce/controllers"
 	"ecommerce/middleware"
 	"github.com/gin-gonic/gin"
+	"github.com/newrelic/go-agent/v3/integrations/nrgin"
 )
 
 func RegisterRoutes(r *gin.Engine) {
 	api := r.Group("/api")
+
+	api.GET("/exemplo", func(c *gin.Context) {
+		txn := nrgin.Transaction(c)
+		txn.AddAttribute("rota", "/exemplo")
+		c.JSON(200, gin.H{"mensagem": "rota monitorada"})
+	})
+
+	api.GET("/produtos", func(c *gin.Context) {
+		txn := nrgin.Transaction(c)
+		txn.AddAttribute("rota", "/produtos")
+		txn.AddAttribute("usuario_id", "123")
+		c.JSON(200, gin.H{"mensagem": "Produtos listados"})
+	})
 
 	api.POST("/register", controllers.Register)
 	api.POST("/login", controllers.Login)
